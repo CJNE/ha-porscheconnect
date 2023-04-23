e49e4ed642c250ef64d429565827a179d7598707
Johan Isacsson <johan@generatorhallen.se>
2023-04-23 14:33:54 +0200

    Version 0.0.13

--- custom_components/porscheconnect/const.py
+++ custom_components/porscheconnect/const.py
@@ -27,7 +27,7 @@ PORSCHE_COMPONENTS = [
 
 NAME = "porscheconnect"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.11"
+VERSION = "0.0.13"
 ISSUE_URL = "https://github.com/cjne/ha-porscheconnect/issues"
 
 STARTUP_MESSAGE = f"""
