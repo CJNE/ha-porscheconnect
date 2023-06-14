221978651c17d346d6388a546cebb2d9ec6815d8
Johan Isacsson <johan@generatorhallen.se>
2023-06-14 23:36:33 +0200

    Updated pyporscheconnectapi with support for new auth backend

--- custom_components/porscheconnect/const.py
+++ custom_components/porscheconnect/const.py
@@ -27,7 +27,7 @@ PORSCHE_COMPONENTS = [
 
 NAME = "porscheconnect"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.13"
+VERSION = "0.0.14"
 ISSUE_URL = "https://github.com/cjne/ha-porscheconnect/issues"
 
 STARTUP_MESSAGE = f"""
