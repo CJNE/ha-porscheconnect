ab8af85239d72f6c85a8372f30ffd41aad988fab
Johan Isacsson <johan@generatorhallen.se>
2023-10-29 14:36:18 +0100

    Update const.py version

--- custom_components/porscheconnect/const.py
+++ custom_components/porscheconnect/const.py
@@ -27,7 +27,7 @@ PORSCHE_COMPONENTS = [
 
 NAME = "porscheconnect"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.0.16"
+VERSION = "0.0.17"
 ISSUE_URL = "https://github.com/cjne/ha-porscheconnect/issues"
 
 STARTUP_MESSAGE = f"""
